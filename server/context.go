5cd84b3f344a9ec0aa15a6c730decc329f795c63
Ripta Pasay <ripta@users.noreply.github.com>
2018-01-04 06:40:46 +0100

    server: add context manipulation helpers

---
+++ server/context.go
@@ -0,0 +1,44 @@
+package server
+
+import (
+	"context"
+	"net/http"
+)
+
+type contextKey int
+
+const (
+	contextKeyBucket contextKey = iota
+	contextKeyObjectKey
+)
+
+func Bucket(r *http.Request) string {
+	if rv := r.Context().Value(contextKeyBucket); rv != nil {
+		return rv.(string)
+	}
+	return ""
+}
+
+func ObjectKey(r *http.Request) string {
+	if rv := r.Context().Value(contextKeyObjectKey); rv != nil {
+		return rv.(string)
+	}
+	return ""
+}
+
+func SetBucket(r *http.Request, name string) *http.Request {
+	ctx := context.WithValue(r.Context(), contextKeyBucket, name)
+	return r.WithContext(ctx)
+}
+
+func SetBucketAndObjectKey(r *http.Request, bucket, key string) *http.Request {
+	ctx := r.Context()
+	ctx = context.WithValue(ctx, contextKeyBucket, bucket)
+	ctx = context.WithValue(ctx, contextKeyObjectKey, key)
+	return r.WithContext(ctx)
+}
+
+func SetObjectKey(r *http.Request, key string) *http.Request {
+	ctx := context.WithValue(r.Context(), contextKeyObjectKey, key)
+	return r.WithContext(ctx)
+}
