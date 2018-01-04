7512cadc0959c616f4f912b9ff1b30fa45570d18
Ripta Pasay <ripta@users.noreply.github.com>
2018-01-04 03:42:13 +0100

    server: connect to s3 and request file

--- server/handler.go
+++ server/handler.go
@@ -1,27 +1,49 @@
 package server
 
 import (
-	"io"
 	"net/http"
 
-	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/s3manager"
 )
 
 type handler struct {
-	s *session.Session
+	cfg aws.Config
+	cli *s3.S3
+	dl  *s3manager.Downloader
 }
 
-func NewHandler() (*handler, error) {
-	s, err := session.NewSession()
+// NewHandler creates a new HTTP handler under the default session configuration
+func NewHandler() (http.Handler, error) {
+	cfg, err := external.LoadDefaultAWSConfig()
 	if err != nil {
 		return nil, err
 	}
 
+	cli := s3.New(cfg)
 	return &handler{
-		s: s,
+		cfg: cfg,
+		cli: cli,
+		dl:  s3manager.NewDownloaderWithClient(cli),
 	}, nil
 }
 
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "hello world!\n")
+	// io.WriteString(w, "hello world!\n")
+	s3req := &s3.GetObjectInput{
+		Bucket: aws.String("userdir-routed-cloud"),
+		Key:    aws.String(r.URL.Path),
+	}
+	buf := &aws.WriteAtBuffer{}
+	n, err := h.dl.DownloadWithContext(r.Context(), buf, s3req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	if n == 0 {
+		http.Error(w, http.StatusText(http.StatusNoContent), http.StatusNoContent)
+		return
+	}
 }
